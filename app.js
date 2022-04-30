797284d589510c843acabc8189430a6fcfcff2dd
‘AMAYA <aamaya547@gmail.com>
2022-04-29 17:20:50 -0700

    final fixes

--- app.js
+++ app.js
@@ -1,35 +1,58 @@
+const cityDropdown = document.getElementById('city-dropdown');
+const mediumDropdown = document.getElementById('medium-dropdown');
+const hustleDropdown = document.getElementById('hustle-dropdown');
+const cityEl = document.getElementById('city');
+const mediumEl = document.getElementById('medium');
+const hustleEl = document.getElementById('hustle');
+// const themeInput = document.getElementById('theme');
+// const themeButton = document.getElementById('save-button');
+const themeEl = document.getElementById('soundtrack');
 
 
+let cityReport = 0;
+let mediumReport = 0;
+let hustleReport = 0;
+// let themes = [];
 
+cityDropdown.addEventListener('change', () => {
+    // console.log('changing select', cityDropdown.value);
+    cityReport++;
+    // console.log(cityReport);
+    cityEl.style.backgroundImage = `url('./assets/cities/${cityDropdown.value}.jpg')`;
+    displayStats();
+});
 
+mediumDropdown.addEventListener('change', () => {
+  // console.log('changing select', mediumDropdown.value);
+    mediumReport++;
+  // console.log(mediumReport);
+    mediumEl.style.backgroundImage = `url('./assets/mediums/${mediumDropdown.value}.jpg')`;
+    displayStats();
+});
 
+hustleDropdown.addEventListener('change', () => {
+  // console.log('changing select', hustleDropdown.value);
+    hustleReport++;
+  // console.log(hustleReport);
+    hustleEl.style.backgroundImage = `url('./assets/hustle/${hustleDropdown.value}.jpg')`;
+    displayStats();
+});
 
+function displayStats() {
+    themeEl.textContent = `You have changed the city ${cityReport} times.You have changed the city ${mediumReport} times. You have changed the side jobs ${hustleReport} times.` ;
+}
 
+// themeButton.addEventListener('click', () => {
+//   themes.push(catchphraseInput.value);
+//   // console.log('changing input', catchphraseInput.value);
+//   // console.log(catchphrases);
+// displayThemes();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import functions and grab DOM elements
-
-// let state
-
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+// function displayThemes() {
+//     themeEl.textContent = '';
+//     for (let theme of themes) {
+//         const li = document.createElement('div');
+//         li.textContent = theme;
+//         themeEl.append(li);
+//     }
+// }
